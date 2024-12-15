b0dcdbb8ef31fde20ad8ffdc102aa5d9e452a10d
Femi Mehalayese <megafemclick@gmail.com>
2024-12-15 16:51:57 +0100

    PRIME GAME 3

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -11,8 +11,32 @@ def isWinner(x, nums):
         x`: numbers of rounds
         nums: array of n integers
     """
-    if x == 0 or x == 1:
+    B = 0
+    M = 0
+    count = 0
+    if x < 1 or nums is None:
         return None
-    if x % 2 == 0:
-        return "Ben"
-    return "Maria"
+    for n in nums:
+        if count > x:
+            break
+        if n == 2:
+            B += 1
+            continue
+        prime = 0
+        for r in range(2, n+1):
+            if r == 2:
+                prime += 1
+                continue
+            elif r % 2 != 0:
+                prime += 1
+        if prime % 2 == 0:
+            M += 1
+        else:
+            B += 1
+        count += 1
+    if B > M:
+        return 'Maria'
+    if M > B:
+        return 'Ben'
+    else:
+        return None
