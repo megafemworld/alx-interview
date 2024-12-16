032958eb8a8df269910a8a9bcf6345c730455686
Femi Mehalayese <megafemclick@gmail.com>
2024-12-17 00:06:57 +0100

    island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for island_perimeter"""
+
+def island_perimeter(grid):
+    """
+    island_perimeter: returns the perimeter of the island described in grid
+
+    Args:
+        grid (_type_): _description_
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
